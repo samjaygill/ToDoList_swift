a99a137071409e28d8ce3c4addac8602373e4e43
samjaygill <138401809+samjaygill@users.noreply.github.com>
2024-02-21 13:06:05 +0000

    log in page completed

--- ToDoList/Views/LogInView.swift
+++ ToDoList/Views/LogInView.swift
@@ -8,8 +8,48 @@
 import SwiftUI
 
 struct LogInView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            VStack{
+                // Header
+                HeaderView()
+                // Login Form
+                Form {
+                    TextField("Email Address", text: $email)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                    SecureField("Password", text: $password)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                    
+                    Button{
+                        // attempt log in
+                    } label: {
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 10)
+                                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                            Text("Log In")
+                                .foregroundColor(.white)
+                                .bold()
+                        }
+                    }
+                    .padding()
+                }
+                // Create Account
+                VStack{
+                    Text("New here?")
+                   
+                        // show registration
+                    NavigationLink("Create An Account",
+                                   destination: RegisterView())
+                }
+                .padding(.bottom, 50)
+                
+                Spacer()
+            }
+        }
     }
 }
 
