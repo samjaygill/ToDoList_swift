2908df543ca4cd72d5db4a8dcdfb36cb0061773f
samjaygill <138401809+samjaygill@users.noreply.github.com>
2024-02-21 11:28:55 +0000

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Samantha Gillies on 21/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
